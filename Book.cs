4d7a2650a8cac39ae9a39f9036bfac753e34998e
Luis.Pedro.Fonseca <Luis.Pedro.Fonseca@outlook.com>
2019-11-10 17:44:46 +0000

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookGrade
+{
+    class Book
+    {
+        // Explicit constructor:
+        public Book(string name)
+        {
+            // Initialization of the field named grades
+            // Always be initialized
+            grades = new List<double>();
+            this.name = name;
+        }    
+
+        //Instant method
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);  
+        }
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var HighGrade = double.MinValue;
+            var LowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                HighGrade = Math.Max(HighGrade, number);
+                LowGrade = Math.Min(LowGrade, number);
+                result += number;
+            }
+            result /= grades.Count;
+            // Console class has static method WriteLine
+            Console.WriteLine($"Average: {result:N2}");
+            Console.WriteLine($"Highest Grade: {HighGrade:N2}");
+            Console.WriteLine($"Lowest Grade: {LowGrade:N2}");
+        }
+
+        // (Instant) Field named grades
+        private List<double> grades;
+        private string name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book2  = null;
+            if (book2 == null)
+                Console.WriteLine("Null values detected");
+
+            var book = new Book("Luis Grade Book");
+            book.AddGrade(10.45);
+            book.AddGrade(19);
+            book.ShowStatistics();
+
+            /*var grades = new List<double>() { 12.25, 15.5, 20.5 }; //This is an initialization expression.
+            grades.Add(10);*/
+
+
+
+        }
+    }
+}
